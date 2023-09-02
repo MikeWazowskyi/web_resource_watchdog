e3be1ac06530fdeb4448716bd27710611d45d709
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-02 15:36:46 +0300

    feat: API error created.

---
+++ web_resource_watchdog/errors/api.py
@@ -0,0 +1,17 @@
+from http import HTTPStatus
+
+
+class InvalidAPIUsage(Exception):
+    """Invalid API usage exception."""
+
+    status_code = HTTPStatus.NOT_FOUND
+
+    def __init__(self, message, status_code=None):
+        super().__init__()
+        self.message = message
+        if status_code:
+            self.status_code = status_code
+
+    def to_dict(self):
+        """Convert InvalidAPIUsage instance to dictionary."""
+        return dict(error=self.message)
