7e5cfc4fc2335701bbd5a03866ffb91cb3e7b06e
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-02 15:11:17 +0300

    feat: api_v1 blueprint created and registered.

--- web_resource_watchdog/__init__.py
+++ web_resource_watchdog/__init__.py
@@ -1,5 +1,15 @@
-from .factories import create_database, create_flask_app  # noqa D104
+from flask import Blueprint
+
+from .factories import create_database, create_flask_app
 from .settings import Config
 
 flask_app = create_flask_app(Config)
 db, migrate = create_database(flask_app)
+
+api_v1 = Blueprint(
+    name="api_v1",
+    url_prefix="/api/v1",
+    import_name=__name__,
+)
+
+flask_app.register_blueprint(api_v1)
