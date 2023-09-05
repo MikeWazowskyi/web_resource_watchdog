727400fb918e29757f0c0fd608c86ea192ef7122
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-05 23:02:09 +0300

    feat: celery application instance added to flask.

--- web_resource_watchdog/__init__.py
+++ web_resource_watchdog/__init__.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
 
 from .factories import create_database, create_flask_app
+from .factories.celery_factory import create_celery_app
 from .settings import Config
 
 flask_app = create_flask_app(Config)
+celery_app = create_celery_app(flask_app)
+flask_app.extensions["celery"] = celery_app
 db, migrate = create_database(flask_app)
 
 api_v1 = Blueprint(
