8dc35387e5f11f4845173280b43fbc084cb2ef7c
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-08 01:33:05 +0300

    feat: task ignore result setting excluded, allowed archives extensions added.

--- web_resource_watchdog/settings.py
+++ web_resource_watchdog/settings.py
@@ -9,15 +9,20 @@ class Config:
     """Application configuration class."""
 
     FLASK_ENV = os.getenv("FLASK_ENV", "development")
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "SQLALCHEMY_DATABASE_URI",
-        "sqlite:///db.sqlite3",
-    )
+    if FLASK_ENV == "development":
+        SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
+    else:
+        SQLALCHEMY_DATABASE_URI = os.getenv(
+            "SQLALCHEMY_DATABASE_URI",
+            "sqlite:///db.sqlite3",
+        )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     FLASK_APP = os.getenv("FLASK_APP", "web_resource_watchdog")
     CELERY = dict(
         broker_url=os.getenv("BROKER_URL_HOST", "redis://localhost"),
         result_backend=os.getenv("BACKEND_RESULT_HOST", "redis://localhost"),
-        task_ignore_result=True,
     )
+    ALLOWED_ARCHIVES_EXTENSIONS = {
+        "zip",
+    }
