3f7324924173e239ba235ea777281d1176987bba
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-08 01:05:54 +0300

    feat: Pydantic validator for zip files added.

--- web_resource_watchdog/schemas/resource.py
+++ web_resource_watchdog/schemas/resource.py
@@ -1,7 +1,51 @@
-from pydantic import BaseModel, HttpUrl
+import io
+import zipfile
+
+from pydantic import BaseModel, HttpUrl, ValidationError, field_validator
+
+from web_resource_watchdog import Config
 
 
 class CreateWebResource(BaseModel):
     """Pydantic schema for web resource model."""
 
     full_url: HttpUrl
+
+
+class ZipFileValidator(BaseModel):
+    """Pydantic schema for zip-file validation."""
+
+    zip_file: bytes
+
+    @field_validator("zip_file")
+    def validate_zip_file(cls, value):
+        """Validate the uploaded zip file.
+
+        Parameters:
+            value (bytes): The uploaded zip file as bytes.
+
+        Returns:
+            io.BytesIO: A BytesIO object representing
+            the validated zip buffer.
+
+        Raises:
+            ValidationError: If the uploaded file is not a valid
+            zip archive.
+        """
+        zip_buffer = io.BytesIO(value)
+        try:
+            with zipfile.ZipFile(zip_buffer, "r") as zip_ref:
+                zip_ref.testzip()
+            return zip_buffer
+        except zipfile.BadZipFile:
+            raise ValidationError(
+                "Invalid zip file",
+            )
+
+
+def allowed_file(filename):
+    """Check file extension in allowed extensions."""
+    return ("." in filename) and (
+        filename.rsplit(".", 1)[1].lower()
+        in Config.ALLOWED_ARCHIVES_EXTENSIONS
+    )
