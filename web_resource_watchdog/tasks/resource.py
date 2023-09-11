f268d6d77ea41d1c390d28dbcad3d8320c9b6dee
Михаил Касаткин <m.m.kasatkin@yandex.ru>
2023-09-11 13:38:10 +0300

    refactoring: zip file parsing moved to separate util.

--- web_resource_watchdog/tasks/resource.py
+++ web_resource_watchdog/tasks/resource.py
@@ -1,46 +1,15 @@
-import io
 import re
-import zipfile
 
 from celery import shared_task
 
 from web_resource_watchdog.models import WebResource
-
-URL_PATTERN = re.compile(
-    (
-        r"\b(?:http|ftp|https)://(?:[\w_-]+(?:(?:\.[\w_-]+)+))"
-        r"(?:[\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])\b"
-    )
-)
+from web_resource_watchdog.utils.zipfile import parse_zip_file
 
 
 @shared_task
-def parse_zip_file(file: bytes, pattern: re.Pattern = None) -> dict[str, list]:
-    """Parse a zip file containing text files and extracts URLs."""
-    if pattern is None:
-        pattern = URL_PATTERN
-    zip_buffer = io.BytesIO(file)
-    result = set()
-    errors = []
-    with zipfile.ZipFile(zip_buffer, "r") as zipf:
-        for file in zipf.infolist():
-            if file.is_dir():
-                continue
-            with zipf.open(file) as file_in_zip:
-                try:
-                    file_data = file_in_zip.read().decode("utf-8")
-                except UnicodeDecodeError:
-                    errors.append(f"Cannot decode file {file.filename}.")
-                else:
-                    finds = pattern.findall(file_data)
-                    if finds:
-                        result.update(finds)
-                    else:
-                        errors.append(f"File {file.filename} has no urls.")
-    parse_data = {"data": list(result)}
-    if errors:
-        parse_data["errors"] = errors
-    return parse_data
+def find_resources(file: bytes, pattern: re.Pattern = None) -> dict[str, list]:
+    """Find all urls in file."""
+    return parse_zip_file(file, pattern)
 
 
 @shared_task
